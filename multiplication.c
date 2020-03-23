7b4ef23bdf9312c4ed84c8a781fd4be12c3eae2f
eugeniekara <eugenie.karadjoff@gmail.com>
2020-03-23 07:08:28 +0100

    modification signaux

--- multiplication.c
+++ multiplication.c
@@ -1,19 +1,34 @@
-#include "multiplication.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+#include <setjmp.h>
 
-int multi_random()
+int random()
 {
   return rand()%10+1;
 }
 
-void multi_sortie()
+int catch_signal(int sig, void (*handler)(int))
+{
+  structure sigaction action;
+  action.sa_handlher = handler;
+  sygemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  return sigaction(sig, action, NULL);
+}
+
+void sortie(int sig)
 {
   printf("Votre Score : %d\n", score);
   exit(0);
+}
+
+void alarm(int sig)
+{
+  printf("Temps ecoule");
+  raise(SIGINT);
 }
 
 int main()
@@ -22,13 +37,14 @@ int main()
   int score = 0;
   srand(time(NULL));
 
-/** reste le sigaction à insérer **/
+  catch_signal(SIGQUIT, sortie);
+  catch_alarm(SIGINT, alarm);
 
   while(1)
   {
     alarm(5);
-    a = multi_random();
-    b = multi_random();
+    a = random();
+    b = random();
     printf("Resultat de %d x %d ? \n", a, b);
     scanf("%d", &reponse)
 
