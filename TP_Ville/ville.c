b19b77fb32bc66f5d194868cc24d4bca96ea4edb
eugeniekara <eugenie.karadjoff@gmail.com>
2020-03-25 18:06:11 +0100

    extraction et copie dans liste

--- TP_Ville/ville.c
+++ TP_Ville/ville.c
@@ -4,31 +4,41 @@
 
 Liste ville_liste_initialiser()
 {
-
+  return(NULL);
 }
 
-Liste ville_liste_teteinserer(Liste l, int e)
+Liste ville_liste_inserer_elt(Liste l, Element elt)
 {
-
-}
-
-Liste ville_liste_queue_inserer(Liste l, int e)
-{
-
+  Liste v;
+  v = (Liste) malloc(sizeof(Element));
+  if (v == NULL) {
+    printf("Allocation memoire impossible\n");
+    exit(1);
+  }
+  v->val = elt;
+  v->suc = l;
+  return (v);
 }
 
 int main()
 {
-  Liste villes;
-  
-  FILE* fichier = NULL;
+  char ville[CHAR_MAX];
+  long cp;
 
+  villes.ville_liste_initialiser();
+
+  FILE* fichier = NULL;
   fichier = fopen("./villes.txt", "r");
 
   while(fscanf(fichier)!= NULL)
   {
-
-
+    Liste villes;
+    Element elt;
+    fgets(fichier, "%s", &ville);
+    fgets(fichier, "%ld", &cp);
+    strcopy(ville, elt.ville);
+    strcopy(cp, elt.code_postal);
+    ville_liste_inserer_elt(villes, elt);
   }
 
   fclose(fichier);
--- TP_Ville/ville.h
+++ TP_Ville/ville.h
@@ -2,20 +2,17 @@
 #define _ville
 
 #define CHAR_MAX 50
-#define
 
 typedef struct Elt {
-  char ville[CHAR_MAX];
+  char nom_ville[CHAR_MAX];
   long code_postal;
-  struct element *successeur;
+  struct Elt *successeur;
 } Element ;
 
 typedef struct Elt *Liste;
 
 Liste ville_liste_initialiser();
 
-Liste ville_liste_teteinserer(Liste l, int e);
-
-Liste ville_liste_queue_inserer(Liste l, int e);
+Liste ville_liste_inserer_elt();
 
 #endif
